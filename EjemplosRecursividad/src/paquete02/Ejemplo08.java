43ef38f025e81ee4221439855c361b7c1a53c8ed
Erick gaona <Erickgaona625@gmail.com>
2023-01-11 15:52:31 -0500

    Ejercicio en clase

--- EjemplosRecursividad/src/paquete02/Ejemplo08.java
+++ EjemplosRecursividad/src/paquete02/Ejemplo08.java
@@ -4,9 +4,31 @@
  */
 package paquete02;
 
+import java.util.Scanner;
+
 public class Ejemplo08 {
 
     public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese el tamaño del arreglo");
+        int opcion = entrada.nextInt();
+        
+        int[] arreglo = new int[opcion];
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.println("Ingrese los valores para el arreglo");
+            arreglo[i] = entrada.nextInt();
+            
+            
+        }
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.printf("La suma de los numeros es :%s",arreglo[i]+ arreglo[i] );
+            
+        }
+        
+            
+        
+        
+
         // Ingresar por teclado el tamaño de un arreglo
         // Ingresar por teclado los valores enteros del arreglo
         // Sumar los valores del arreglo, mediante la función misterio
